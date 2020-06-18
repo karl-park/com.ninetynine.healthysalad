1dde95645d7e999612cdcbc1897bba3603db3e95
danielcyc <daniel.c.y.cyc@gmail.com>
2020-06-19 01:52:47 +0800

    fix retrofit

--- data/src/main/java/com/example/data/Data.kt
+++ data/src/main/java/com/example/data/Data.kt
@@ -1,15 +1,22 @@
 package com.example.data
 
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.create
 
 
 class Data {
     // to put data into classes
-    
+    lateinit var items: List<FoodType>
+
     var retrofit: Retrofit = Retrofit.Builder()
         .baseUrl("https://raw.githubusercontent.com/karl-park/com.ninetynine.healthysalad/master/server/api/")
         .build()
 
     var service: DataGetter = retrofit.create(DataGetter::class.java)
+
+    var base: Call<List<Item>> = service.listItems("base")
+    var ingredients  = listOf<String>("base", "crunchy", "dressing","protein","soft")
+
+    for (item in ingredients)
 }
--- data/src/main/java/com/example/data/FoodType.kt
+++ data/src/main/java/com/example/data/FoodType.kt
@@ -1,6 +1,6 @@
 package com.example.data
 
 data class FoodType (
-    val item: Item
+    val item: List <Item>
 ) {
 }
