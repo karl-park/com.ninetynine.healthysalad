ebfbdf0d66a6825d100a636d17ee5532ddc0e7af
danielcyc <daniel.c.y.cyc@gmail.com>
2020-07-28 21:24:44 +0800

    change order class to singleton object

--- presentation/src/main/java/com/example/healthysalad/MainActivity.kt
+++ presentation/src/main/java/com/example/healthysalad/MainActivity.kt
@@ -11,11 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //Data().loadAllItems()
-        //var repo: MutableMap<String, List<Base>> = mutableMapOf()
-        val order = Order()
-
         var titleFragmentInstance : TitleFragment = TitleFragment.newInstance()
         this.supportFragmentManager.beginTransaction()
             .replace(R.id.activity_main, titleFragmentInstance)
